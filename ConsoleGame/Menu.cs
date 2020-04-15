dff8175b3fba206175c23fb07b0f89e7e618edc4
Orlovich Dario <orlovichdario@teksistemi.com>
2020-04-15 14:32:36 +0100

    ch4

--- ConsoleGame/Menu.cs
+++ ConsoleGame/Menu.cs
@@ -41,7 +41,7 @@ namespace ConsoleGame
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine();
             //debug: start from
-            NodeFactory.CreateNode("3_09");
+            NodeFactory.CreateNode("3_01");
             //debug
 
 
