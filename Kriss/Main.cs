2c1f0d1d6adb7b2be1ef7842f717b45230db39d7
Dario Orlovich <orlodax@gmail.com>
2025-05-03 17:56:13 +0100

    add steamworks

--- Kriss/Main.cs
+++ Kriss/Main.cs
@@ -1,5 +1,32 @@
 ﻿global using static System.Console;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using KrissJourney.Kriss.Classes;
 
 Title = "KRISS' JOURNEY";
 
-KrissJourney.Kriss.Classes.DataLayer.Init();
+#region Steam API Initialization
+SteamManager.Initialize(() =>
+{
+    // This will be called when the application quits
+    Debug.WriteLine("Game shutting down - cleaning up Steam resources");
+});
+
+// Set up a simple callback runner because the game doesn't have a dedicated update loop
+_ = Task.Run(async () =>
+{
+    while (SteamManager.Initialized)
+    {
+        SteamManager.RunCallbacks();
+        await Task.Delay(15);
+    }
+});
+
+// Register an event to handle application exit
+System.AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+{
+    SteamManager.Shutdown();
+};
+#endregion
+
+DataLayer.Init();
