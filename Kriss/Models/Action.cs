eec90290d8dd7ef4aacb8040ecf14571920e21e0
Dario Orlovich <orlodax@gmail.com>
2025-05-22 21:58:58 +0100

    bugfix action node getting help on verbs

--- Kriss/Models/Action.cs
+++ Kriss/Models/Action.cs
@@ -18,42 +18,50 @@ public class Action : IAction
     public List<ActionObject> Objects { get; set; } = []; // objects for the verbs
     public Condition Condition { get; set; } // condition for the viability of the action. normally an item
     public Effect Effect { get; set; } // consequence from the base action
-    public string GetOpinion(string word) // to get response message when action requires an object and player does not provide any valid
+    public string GetAnswer(string word) // to get response message when action requires an object and player does not provide any valid
     {
         if (Answer != null)
             return Answer;
         else
+            return GetHelpObject(word);
+    }
+
+    public static string GetHelpObject(string word) // to get response message when action requires an object and player does not provide any valid
+    {
+        return word switch
         {
-            if (!string.IsNullOrWhiteSpace(word))
-                switch (word)
-                {
-                    case "look":
-                        return "What shoud I look at? Where?";
-                    case "take":
-                        return "What shoud I take?";
-                    case "go":
-                        return "Where should I go?";
-                    case "search":
-                        return "Where should I search? For what?";
-                    case "remove":
-                        return "What will I remove? from where?";
-                    case "wear":
-                        return "What could I wear?";
-                    case "rest":
-                        return "Where could I lay down?";
-                    case "drink":
-                        return "What will I drink?";
-                    case "eat":
-                        return "What will I eat?";
-                    case "sleep":
-                        return "Where can I sleep?";
-                    case "say":
-                        return "What could I say?";
-                    case "ask":
-                        return "What will I ask?";
-                }
-            return "Sorry can't do that.";
-        }
+            "look" => "What shoud I look at? Where?",
+            "take" => "What shoud I take?",
+            "go" => "Where should I go?",
+            "search" => "Where should I search? For what?",
+            "remove" => "What will I remove? from where?",
+            "wear" => "What could I wear?",
+            "rest" => "Where could I lay down?",
+            "drink" => "What will I drink?",
+            "eat" => "What will I eat?",
+            "sleep" => "Where can I sleep?",
+            "say" => "What could I say?",
+            "ask" => "What will I ask?",
+            "give" => "What will I give?",
+            "use" => "What will I use?",
+            "read" => "What will I read?",
+            "write" => "What will I write?",
+            "open" => "What will I open?",
+            "close" => "What will I close?",
+            "unlock" => "What will I unlock?",
+            "lock" => "What will I lock?",
+            "turn" => "What will I turn?",
+            "push" => "What will I push?",
+            "pull" => "What will I pull?",
+            "hit" => "What will I hit?",
+            "kick" => "What will I kick?",
+            "throw" => "What will I throw?",
+            "catch" => "What will I catch?",
+            "climb" => "Where will I climb?",
+            "jump" => "Where will I jump?",
+            "swim" => "Where will I swim?",
+            _ => "Uhm... I don't know about that...",
+        };
     }
 }
 
